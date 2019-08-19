21f2a1ddaeada600b132a7597514e2b89c0973b8
charl <charl@192.168.115.59>
2019-08-19 19:09:35 -0300

    Correções do código.

--- Exercício 2.3/src/br/edu/udc2_3_5/Inteiros.java
+++ Exercício 2.3/src/br/edu/udc2_3_5/Inteiros.java
@@ -4,15 +4,6 @@ import java.util.Scanner;
 
 public class Inteiros {
 	int num;
-	int resto1;
-	int resto2;
-	int resto3;
-	int resto4;
-	int digito1;
-	int digito2;
-	int digito3;
-	int digito4;
-	int digito5;
 	
 	public void Entrada() {
 		Scanner sc = new Scanner (System.in);
@@ -20,17 +11,11 @@ public class Inteiros {
 		System.out.println("Entre com o número de cinco digitos: ");
 		num = sc.nextInt();
 		
-		resto1 = num%10000;
-		resto2 = num%1000;
-		resto3 = num%100;
-		resto4 = num%10;
-		
-		digito1 = num/10000;
-		digito2 = resto1/1000;
-		digito3 = resto2/100;
-		digito4 = resto3/10;
-		digito5 = resto4;
-		
-		System.out.println(digito1 + " " + digito2 + " " + digito3 + " " + digito4 + " " + digito5);	
+		if (num > 9999 && num < 100000) {
+		String str = String.valueOf(num);
+		System.out.println(str.charAt(0) + "   " + str.charAt(1) + "   " + str.charAt(2) + "   " + str.charAt(3) + "   " + str.charAt(4));
+		}
+		else
+			System.out.println("Entre com um número válido!");
 	}
 }
