39365c6312fadd54761d8870ec9ab2349c5bc8a0
charl <charl@DESKTOP-7E5RDR3>
2019-09-11 04:06:36 -0300

    For com do while.

--- Exercício 4.4/src/br/edu/udc4_3_4/TrianguloFloyd.java
+++ Exercício 4.4/src/br/edu/udc4_3_4/TrianguloFloyd.java
@@ -3,24 +3,16 @@ package br.edu.udc4_3_4;
 import java.util.Scanner;
 
 public class TrianguloFloyd {
-	int i = 0;
-	int qtd = 1;
-	int num = 1;
-	int n;
+	int n, i, j;
 	
 	public void ImprimeTriangulo() {
-		if (n < 1)
-			return;
-		
-		while (i < n) {
-			int j;
-			for (j = 0; j < qtd; j++) {
-				System.out.printf("%d \n", num);
-				num++;
-			}
-			System.out.printf("\n");
-			qtd++;
-			i++;
+		for (i = 1; i <= n; i++) {
+			j = 1;
+			
+			do {
+				System.out.printf(j*i + " ");
+				j++;
+			}while(j <= i);
 		}
 	}
 	
