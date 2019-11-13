58039d3085b7ea9e52ccafc4dac63b8096417e4a
charl <charl@DESKTOP-7E5RDR3>
2019-11-12 21:25:25 -0300

    Fixado problema do arrasto do mouse.

--- Exercício 15.4/src/br/edu/udc/ComandosMouse/MouseFrame.java
+++ Exercício 15.4/src/br/edu/udc/ComandosMouse/MouseFrame.java
@@ -1,7 +1,5 @@
 package br.edu.udc.ComandosMouse;
 
-/* CONSERTAR: TRIÂNGULO, CLIQUES NÃO PUXADOS PARA LINHA/QUADRADO/CIRCULO (colocar um reset?) */
-
 import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
