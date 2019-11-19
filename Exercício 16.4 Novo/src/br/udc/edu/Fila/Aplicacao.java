d6c6c4fcbf4c1bd5dc05da1fbe8225cd646eab58
charl <charl@DESKTOP-7E5RDR3>
2019-11-19 20:55:37 -0300

    Feito a Aplicação para demonstrar o funcionamento do código.

---
+++ Exercício 16.4 Novo/src/br/udc/edu/Fila/Aplicacao.java
@@ -0,0 +1,27 @@
+package br.udc.edu.Fila;
+
+import br.edu.udc7_1.Ponto2D;
+
+public class Aplicacao {
+
+	public static void main (String [] args) {
+		Fila<Ponto2D> fila = new Fila<Ponto2D>();
+
+		fila.queue(new Ponto2D(1, 2));
+		fila.queue(new Ponto2D(3, 4));
+		fila.queue(new Ponto2D(5, 6));
+		fila.queue(new Ponto2D(7, 8));
+		fila.queue(new Ponto2D(9, 10));
+		fila.queue(new Ponto2D(11, 12));
+		fila.queue(new Ponto2D(13, 14));
+		fila.queue(new Ponto2D(15, 16));
+
+		System.out.println("Tamanho Fila: " + fila.getTamanho());
+		
+		fila.dequeue();
+		fila.dequeue();
+		fila.dequeue();
+		
+		System.out.println("Tamanho Fila: " + fila.getTamanho());
+	}
+}
